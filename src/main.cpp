7aa6629f6efd8b7071ac9cec494f62f24589d3de
d.dankovskyi <d.dankovskyi@gmail.com>
2024-01-07 20:11:47 +0200

    moved on vs code + fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include "RTClib.h"
+#include <RTClib.h>
 #include <Wire.h>
 
 int lightOnH = 9;   // hour of turning light on
@@ -22,52 +22,60 @@ bool isLightOn = false;
 int humidifierWorkingTimeH = 4;
 int humidifierBreakTimeH = 1;
 
-
 int humOnTimeH = lightOnH;
 int humOffTimeH = lightOnH + humidifierWorkingTimeH;
 bool isHumOn = false;
 
-void setup() {
+void setup()
+{
   Wire.begin();
   Serial.begin(9600);
+
   Serial.println("Hello salad3");
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // set clock time
-  showTime();
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+  // //rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // set clock time
+
+   showTime();
 
   initLight();
   pinMode(humidifierPin, OUTPUT);
 
   updateStatus();
-  Serial.println("light on ");
+  Serial.print("light on ");
   Serial.println(isLightOn);
-  Serial.println("humidifier on ");
+  Serial.print("humidifier on ");
   Serial.println(isHumOn);
-  // put your setup code here, to run once:
 }
 
-void loop () {
+void loop()
+{
+
   updateStatus();
-//
+  
 }
 
-void updateStatus(){
+void updateStatus()
+{
   DateTime now = rtc.now();
 
   isLightOn = now.hour() >= lightOnH && now.hour() < lightOffH;
-  //isLightOn = now.minute()%2 <= 0;
+  // isLightOn = now.minute()%2 <= 0;
   digitalWrite(lightPin, isLightOn);
   updateHumidifier();
   delay(3000);
 }
-
-
 
 void updateHumidifier()
 {
 
   DateTime now = rtc.now();
 
-  if(!isLightOn)
+  if (!isLightOn)
   {
     humOnTimeH = lightOnH;
     humOffTimeH = lightOnH + humidifierWorkingTimeH;
@@ -76,40 +84,38 @@ void updateHumidifier()
     return;
   }
 
-  if(!isHumOn && now.hour() >= humOnTimeH){
+  if (!isHumOn && now.hour() >= humOnTimeH)
+  {
     humOffTimeH = now.hour() + humidifierWorkingTimeH;
-    if(humOffTimeH > lightOffH) humOffTimeH = lightOffH;
+    if (humOffTimeH > lightOffH)
+      humOffTimeH = lightOffH;
     isHumOn = true;
     digitalWrite(humidifierPin, isHumOn);
     return;
   }
 
-  if(isHumOn && now.hour() >= humOffTimeH){
+  if (isHumOn && now.hour() >= humOffTimeH)
+  {
     humOnTimeH = now.hour() + humidifierBreakTimeH;
-    if(humOnTimeH > lightOffH) humOnTimeH = lightOnH;
+    if (humOnTimeH > lightOffH)
+      humOnTimeH = lightOnH;
     isHumOn = false;
     digitalWrite(humidifierPin, isHumOn);
     return;
   }
-
 }
 
-
-void initLight(){
+void initLight()
+{
   pinMode(lightPin, OUTPUT);
 }
 
-void showTime(){
+
+
+void showTime()
+{
   DateTime now = rtc.now();
 
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
   Serial.print(now.hour(), DEC);
   Serial.print(':');
   Serial.print(now.minute(), DEC);
